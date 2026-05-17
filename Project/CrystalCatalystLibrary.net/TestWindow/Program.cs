48c256b8699b47ce6aacc42bfdd979911b179f7a
jwc <john.wayne.cornell@gmail.com>
2026-05-17 15:23:45 -0400

    **CrystalWindow** Windows fixes

--- Project/CrystalCatalystLibrary.net/TestWindow/Program.cs
+++ Project/CrystalCatalystLibrary.net/TestWindow/Program.cs
@@ -1,17 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
 using System.Runtime.InteropServices;
 using CrystalCatalystLibrary.net;
 using JWCEssentials.net;
 using TestWindow;
 
-Console.WriteLine(JWCEssentials.net.Essentials.feffect("fg_red(\"Hello\")"));
+namespace TestWindow
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine(JWCEssentials.net.Essentials.feffect("fg_red(\"Hello\")"));
 
-/*
-utf8_string_struct[] tmp = (from s in args select (utf8_string_struct)s).ToArray();
+            /*
+            utf8_string_struct[] tmp = (from s in args select (utf8_string_struct)s).ToArray();
 
-struct_array_struct<utf8_string_struct> _args =
-    (struct_array_struct<utf8_string_struct>)tmp; */
-Application.Init(args);
-Window wnd = new Window();
-Application.Run();
+            struct_array_struct<utf8_string_struct> _args =
+                (struct_array_struct<utf8_string_struct>)tmp; */
+            Application.Init(args);
+            Window wnd = new Window();
+            Application.Run();
+        }
+    }
+}
