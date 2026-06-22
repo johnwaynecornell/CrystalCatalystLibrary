191f5c86f080f43bf9aaa1bc386efeebc75d4677
jwc <john.wayne.cornell@gmail.com>
2026-06-22 13:30:03 -0400

    **CrystalOpenGL**: Expand GLBridges

--- Project/CrystalCatalystLibrary.net/CrystalOpenGL/GLBridges.cs
+++ Project/CrystalCatalystLibrary.net/CrystalOpenGL/GLBridges.cs
@@ -18,9 +18,16 @@ public static class GLBridges
     private delegate void ReadPixelsDelegate(GL gl, int x, int y, uint width, uint height, PixelFormat format, PixelType type, IntPtr pixels);
     private delegate void DrawBuffersDelegate(GL gl, uint n, IntPtr bufs);
     private delegate void TexImage2DDelegate(GL gl, TextureTarget target, int level, InternalFormat internalformat, uint width, uint height, int border, PixelFormat format, PixelType type, IntPtr pixels);
+    private delegate void TexImage3DDelegate(GL gl, TextureTarget target, int level, InternalFormat internalformat, uint width, uint height, uint depth, int border, PixelFormat format, PixelType type, IntPtr pixels);
     private delegate void TexSubImage2DDelegate(GL gl, TextureTarget target, int level, int xoffset, int yoffset, uint width, uint height, PixelFormat format, PixelType type, IntPtr pixels);
+    private delegate void TexSubImage3DDelegate(GL gl, TextureTarget target, int level, int xoffset, int yoffset, int zoffset, uint width, uint height, uint depth, PixelFormat format, PixelType type, IntPtr pixels);
+    private delegate void CompressedTexImage3DDelegate(GL gl, TextureTarget target, int level, InternalFormat internalformat, uint width, uint height, uint depth, int border, uint imageSize, IntPtr data);
+    private delegate void CompressedTexSubImage3DDelegate(GL gl, TextureTarget target, int level, int xoffset, int yoffset, int zoffset, uint width, uint height, uint depth, InternalFormat format, uint imageSize, IntPtr data);
     private delegate void GetTexImageDelegate(GL gl, TextureTarget target, int level, PixelFormat format, PixelType type, IntPtr pixels);
     private delegate void TextureSubImage2DDelegate(GL gl, uint texture, int level, int xoffset, int yoffset, uint width, uint height, PixelFormat format, PixelType type, IntPtr pixels);
+    private delegate void TextureSubImage3DDelegate(GL gl, uint texture, int level, int xoffset, int yoffset, int zoffset, uint width, uint height, uint depth, PixelFormat format, PixelType type, IntPtr pixels);
+    private delegate void CompressedTextureSubImage3DDelegate(GL gl, uint texture, int level, int xoffset, int yoffset, int zoffset, uint width, uint height, uint depth, InternalFormat format, uint imageSize, IntPtr data);
+    private delegate void GetTextureSubImageDelegate(GL gl, uint texture, int level, int xoffset, int yoffset, int zoffset, uint width, uint height, uint depth, PixelFormat format, PixelType type, uint bufSize, IntPtr pixels);
     private delegate void GetTextureImageDelegate(GL gl, uint texture, int level, PixelFormat format, PixelType type, uint bufSize, IntPtr pixels);
     private delegate void DrawElementsDelegate(GL gl, PrimitiveType mode, uint count, DrawElementsType type, IntPtr indices);
 
@@ -31,9 +38,16 @@ public static class GLBridges
     private static readonly ReadPixelsDelegate _readPixels;
     private static readonly DrawBuffersDelegate _drawBuffers;
     private static readonly TexImage2DDelegate _texImage2D;
+    private static readonly TexImage3DDelegate _texImage3D;
     private static readonly TexSubImage2DDelegate _texSubImage2D;
+    private static readonly TexSubImage3DDelegate _texSubImage3D;
+    private static readonly CompressedTexImage3DDelegate _compressedTexImage3D;
+    private static readonly CompressedTexSubImage3DDelegate _compressedTexSubImage3D;
     private static readonly GetTexImageDelegate _getTexImage;
     private static readonly TextureSubImage2DDelegate _textureSubImage2D;
+    private static readonly TextureSubImage3DDelegate _textureSubImage3D;
+    private static readonly CompressedTextureSubImage3DDelegate _compressedTextureSubImage3D;
+    private static readonly GetTextureSubImageDelegate _getTextureSubImage;
     private static readonly GetTextureImageDelegate _getTextureImage;
     private static readonly DrawElementsDelegate _drawElements;
 
@@ -45,9 +59,16 @@ public static class GLBridges
         _readPixels = CreateDelegate<ReadPixelsDelegate>("ReadPixels", typeof(int), typeof(int), typeof(uint), typeof(uint), typeof(PixelFormat), typeof(PixelType), typeof(void*));
         _drawBuffers = CreateDelegate<DrawBuffersDelegate>("DrawBuffers", typeof(uint), typeof(GLEnum*));
         _texImage2D = CreateDelegate<TexImage2DDelegate>("TexImage2D", typeof(TextureTarget), typeof(int), typeof(InternalFormat), typeof(uint), typeof(uint), typeof(int), typeof(PixelFormat), typeof(PixelType), typeof(void*));
+        _texImage3D = CreateDelegate<TexImage3DDelegate>("TexImage3D", typeof(TextureTarget), typeof(int), typeof(InternalFormat), typeof(uint), typeof(uint), typeof(uint), typeof(int), typeof(PixelFormat), typeof(PixelType), typeof(void*));
         _texSubImage2D = CreateDelegate<TexSubImage2DDelegate>("TexSubImage2D", typeof(TextureTarget), typeof(int), typeof(int), typeof(int), typeof(uint), typeof(uint), typeof(PixelFormat), typeof(PixelType), typeof(void*));
+        _texSubImage3D = CreateDelegate<TexSubImage3DDelegate>("TexSubImage3D", typeof(TextureTarget), typeof(int), typeof(int), typeof(int), typeof(int), typeof(uint), typeof(uint), typeof(uint), typeof(PixelFormat), typeof(PixelType), typeof(void*));
+        _compressedTexImage3D = CreateDelegate<CompressedTexImage3DDelegate>("CompressedTexImage3D", typeof(TextureTarget), typeof(int), typeof(InternalFormat), typeof(uint), typeof(uint), typeof(uint), typeof(int), typeof(uint), typeof(void*));
+        _compressedTexSubImage3D = CreateDelegate<CompressedTexSubImage3DDelegate>("CompressedTexSubImage3D", typeof(TextureTarget), typeof(int), typeof(int), typeof(int), typeof(int), typeof(uint), typeof(uint), typeof(uint), typeof(InternalFormat), typeof(uint), typeof(void*));
         _getTexImage = CreateDelegate<GetTexImageDelegate>("GetTexImage", typeof(TextureTarget), typeof(int), typeof(PixelFormat), typeof(PixelType), typeof(void*));
         _textureSubImage2D = CreateDelegate<TextureSubImage2DDelegate>("TextureSubImage2D", typeof(uint), typeof(int), typeof(int), typeof(int), typeof(uint), typeof(uint), typeof(PixelFormat), typeof(PixelType), typeof(void*));
+        _textureSubImage3D = CreateDelegate<TextureSubImage3DDelegate>("TextureSubImage3D", typeof(uint), typeof(int), typeof(int), typeof(int), typeof(int), typeof(uint), typeof(uint), typeof(uint), typeof(PixelFormat), typeof(PixelType), typeof(void*));
+        _compressedTextureSubImage3D = CreateDelegate<CompressedTextureSubImage3DDelegate>("CompressedTextureSubImage3D", typeof(uint), typeof(int), typeof(int), typeof(int), typeof(int), typeof(uint), typeof(uint), typeof(uint), typeof(InternalFormat), typeof(uint), typeof(void*));
+        _getTextureSubImage = CreateDelegate<GetTextureSubImageDelegate>("GetTextureSubImage", typeof(uint), typeof(int), typeof(int), typeof(int), typeof(int), typeof(uint), typeof(uint), typeof(uint), typeof(PixelFormat), typeof(PixelType), typeof(uint), typeof(void*));
         _getTextureImage = CreateDelegate<GetTextureImageDelegate>("GetTextureImage", typeof(uint), typeof(int), typeof(PixelFormat), typeof(PixelType), typeof(uint), typeof(void*));
         _drawElements = CreateDelegate<DrawElementsDelegate>("DrawElements", typeof(PrimitiveType), typeof(uint), typeof(DrawElementsType), typeof(void*));
     }
@@ -95,8 +116,15 @@ public static class GLBridges
     public static void ReadPixels(GL gl, int x, int y, uint width, uint height, PixelFormat format, PixelType type, IntPtr pixels) => _readPixels(gl, x, y, width, height, format, type, pixels);
     public static void DrawBuffers(GL gl, uint n, IntPtr bufs) => _drawBuffers(gl, n, bufs);
     public static void TexImage2D(GL gl, TextureTarget target, int level, InternalFormat internalformat, uint width, uint height, int border, PixelFormat format, PixelType type, IntPtr pixels) => _texImage2D(gl, target, level, internalformat, width, height, border, format, type, pixels);
+    public static void TexImage3D(GL gl, TextureTarget target, int level, InternalFormat internalformat, uint width, uint height, uint depth, int border, PixelFormat format, PixelType type, IntPtr pixels) => _texImage3D(gl, target, level, internalformat, width, height, depth, border, format, type, pixels);
     public static void TexSubImage2D(GL gl, TextureTarget target, int level, int xoffset, int yoffset, uint width, uint height, PixelFormat format, PixelType type, IntPtr pixels) => _texSubImage2D(gl, target, level, xoffset, yoffset, width, height, format, type, pixels);
+    public static void TexSubImage3D(GL gl, TextureTarget target, int level, int xoffset, int yoffset, int zoffset, uint width, uint height, uint depth, PixelFormat format, PixelType type, IntPtr pixels) => _texSubImage3D(gl, target, level, xoffset, yoffset, zoffset, width, height, depth, format, type, pixels);
+    public static void CompressedTexImage3D(GL gl, TextureTarget target, int level, InternalFormat internalformat, uint width, uint height, uint depth, int border, uint imageSize, IntPtr data) => _compressedTexImage3D(gl, target, level, internalformat, width, height, depth, border, imageSize, data);
+    public static void CompressedTexSubImage3D(GL gl, TextureTarget target, int level, int xoffset, int yoffset, int zoffset, uint width, uint height, uint depth, InternalFormat format, uint imageSize, IntPtr data) => _compressedTexSubImage3D(gl, target, level, xoffset, yoffset, zoffset, width, height, depth, format, imageSize, data);
     public static void TextureSubImage2D(GL gl, uint texture, int level, int xoffset, int yoffset, uint width, uint height, PixelFormat format, PixelType type, IntPtr pixels) => _textureSubImage2D(gl, texture, level, xoffset, yoffset, width, height, format, type, pixels);
+    public static void TextureSubImage3D(GL gl, uint texture, int level, int xoffset, int yoffset, int zoffset, uint width, uint height, uint depth, PixelFormat format, PixelType type, IntPtr pixels) => _textureSubImage3D(gl, texture, level, xoffset, yoffset, zoffset, width, height, depth, format, type, pixels);
+    public static void CompressedTextureSubImage3D(GL gl, uint texture, int level, int xoffset, int yoffset, int zoffset, uint width, uint height, uint depth, InternalFormat format, uint imageSize, IntPtr data) => _compressedTextureSubImage3D(gl, texture, level, xoffset, yoffset, zoffset, width, height, depth, format, imageSize, data);
+    public static void GetTextureSubImage(GL gl, uint texture, int level, int xoffset, int yoffset, int zoffset, uint width, uint height, uint depth, PixelFormat format, PixelType type, uint bufSize, IntPtr pixels) => _getTextureSubImage(gl, texture, level, xoffset, yoffset, zoffset, width, height, depth, format, type, bufSize, pixels);
     public static void GetTexImage(GL gl, TextureTarget target, int level, PixelFormat format, PixelType type, IntPtr pixels) => _getTexImage(gl, target, level, format, type, pixels);
     public static void GetTextureImage(GL gl, uint texture, int level, PixelFormat format, PixelType type, uint bufSize, IntPtr pixels) => _getTextureImage(gl, texture, level, format, type, bufSize, pixels);
     public static void DrawElements(GL gl, PrimitiveType mode, uint count, DrawElementsType type, IntPtr indices) => _drawElements(gl, mode, count, type, indices);
