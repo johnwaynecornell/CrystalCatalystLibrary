9b1c5ee4e68437bf6ceb19e5df2b00d8f92c1e03
jwc <john.wayne.cornell@gmail.com>
2026-09-21 17:07:56 -0400

    **SlideScramble**: Encapsulate `Application.Init` and main logic in `Main` method.

--- Project/CrystalCatalystLibrary.net/SlideScramble/Program.cs
+++ Project/CrystalCatalystLibrary.net/SlideScramble/Program.cs
@@ -1,11 +1,17 @@
-﻿
 using CrystalCatalystLibrary.net;
 using SlideScramble;
 
-Application.Init(args);
+class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        Application.Init(args);
 
-Window wnd = new Window();
+        Window wnd = new Window();
 
-wnd.Show(true);
-Application.SetDiagnosticsCallback((msg) => { });
-Application.Run();
+        wnd.Show(true);
+        Application.SetDiagnosticsCallback((msg) => { });
+        Application.Run();
+    }
+}
